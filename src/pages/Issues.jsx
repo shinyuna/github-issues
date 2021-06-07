a69dc1cd556553d0c03efe99d2c882456d2a728c
신유나 <yunadev01@gmail.com>
2021-06-07 17:13:54 +0900

    Update issue component

--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { API } from '../api';
+import { Issue } from '../components/Issue';
 
 export const Issues = () => {
   const [issues, setIssues] = useState([]);
@@ -44,22 +45,16 @@ export const Issues = () => {
               </a>
             </div>
           ) : (
-            <div className="issue" key={issue.id}>
-              <div className="issue__info">
-                <p className="issue__info-title">{issue.title}</p>
-                <p className="issue__info-sub">
-                  #{issue.number} {new Date(issue.createdAt).toLocaleDateString('ko')} by {issue.writer.login}
-                </p>
-              </div>
-              <div className="issue__comment">
-                <svg viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-                  <path
-                    fillRule="evenodd"
-                    d="M2.75 2.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h2a.75.75 0 01.75.75v2.19l2.72-2.72a.75.75 0 01.53-.22h4.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25H2.75zM1 2.75C1 1.784 1.784 1 2.75 1h10.5c.966 0 1.75.784 1.75 1.75v7.5A1.75 1.75 0 0113.25 12H9.06l-2.573 2.573A1.457 1.457 0 014 13.543V12H2.75A1.75 1.75 0 011 10.25v-7.5z"></path>
-                </svg>
-                <span>{issue.comments}</span>
-              </div>
-            </div>
+            <>
+              <Issue
+                key={issue.id}
+                title={issue.title}
+                number={issue.number}
+                createdAt={issue.createdAt}
+                writer={issue.writer.login}
+                comments={issue.comments}
+              />
+            </>
           )
         )}
     </div>
