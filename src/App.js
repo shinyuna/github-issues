1481feb6d7f6615237e2a94808ac26158ef7801c
신유나 <yunadev01@gmail.com>
2021-06-15 17:30:40 +0900

    Update App - delete router file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
-import { Router } from './routers';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Issues } from './pages/Issues';
+import { Header } from './components/Header';
+import { Detail } from './pages/Detail';
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <Header team="angular" repo="angular-cli" />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Issues />
+          </Route>
+          <Route path="/detail/:issue_number" exact>
+            <Detail />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
