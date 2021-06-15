743c1c6e9b4e69577660b132f4107446f9b50c1b
신유나 <yunadev01@gmail.com>
2021-06-15 17:36:11 +0900

    Update useFetch hook - 페이지 뒤로가기 시 전 스크롤 위치로 이동할 수 있도록 수정.. ** 이렇게 하는 것이 맞는걸까... 다른 방법 좀 공유해주세요 고수님들 🥲

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,15 +1,66 @@
 import { useCallback, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import { API } from '../api';
+import { useScrollMove } from './useScrollMove';
 
-export const useFetch = page => {
+export const useFetch = (page, itemName) => {
+  const history = useHistory();
+
+  const [first, setFirst] = useState(true);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [list, setList] = useState([]);
+  const { scrollOnceMove } = useScrollMove();
+
+  const formatData = useCallback(data => {
+    return data.map(issue => {
+      return {
+        id: issue.id,
+        number: issue.number,
+        title: issue.title,
+        writer: issue.user,
+        comments: issue.comments,
+        link: issue.url,
+        createdAt: issue.created_at,
+      };
+    });
+  }, []);
+
+  const callAPI = useCallback(
+    async page => {
+      try {
+        let tempList = [];
+        const arr = Array.from({ length: page });
+        for (let i = 1; i <= arr.length; i++) {
+          console.log(i);
+          const { data } = await API.getRepoIssues('angular', 'angular-cli', {
+            sort: 'comments',
+            page: i,
+            per_page: 10,
+          });
+          tempList = [...tempList, ...data];
+        }
+        const response = await formatData(tempList);
+        sessionStorage.removeItem(itemName);
+        setList(prev => [...prev, ...response]);
+        setFirst(false);
+        scrollOnceMove();
+      } catch (err) {
+        setError(err);
+      }
+    },
+    [formatData, scrollOnceMove, itemName]
+  );
 
   const sendQuery = useCallback(async () => {
     try {
-      await setLoading(true);
-      await setError(false);
+      if (first && page > 1) {
+        return;
+      }
+      if (page) {
+        setLoading(true);
+        setError(false);
+      }
       const { data } = await API.getRepoIssues('angular', 'angular-cli', {
         sort: 'comments',
         page: page,
@@ -18,27 +69,33 @@ export const useFetch = page => {
       if (data.length === 0) {
         throw new Error('Done');
       }
-      const response = data.map(issue => {
-        return {
-          id: issue.id,
-          number: issue.number,
-          title: issue.title,
-          writer: issue.user,
-          comments: issue.comments,
-          link: issue.url,
-          createdAt: issue.created_at,
-        };
-      });
-      await setList(prev => [...prev, ...response]);
+      sessionStorage.removeItem(itemName);
+      setList(prev => [...prev, ...formatData(data)]);
+      setFirst(false);
       setLoading(false);
     } catch (err) {
       setError(err);
     }
+  }, [first, page, itemName, formatData]);
+
+  const savePage = () => {
+    return sessionStorage.setItem(itemName, page);
+  };
+
+  useEffect(() => {
+    if (first && page > 1) {
+      callAPI(page);
+    } else {
+      sendQuery();
+    }
   }, [page]);
 
   useEffect(() => {
-    sendQuery();
-  }, [page, sendQuery]);
+    const listen = history.listen(savePage);
+    return () => {
+      listen();
+    };
+  });
 
   return { loading, error, list };
 };
